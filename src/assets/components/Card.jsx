b60188cb2a2dd4c99c97837fb87cff7fe68ae6b0
MarcoProstamo <Marcopros1201@gmail.com>
2024-12-06 14:39:12 +0100

    Add → Modal Delete

--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -12,10 +12,52 @@ export default function Card({ card, handleClick }) {
           <p className="card-text flex-grow-1">{card.content}</p>
           <button
             className="btn btn-danger"
-            onClick={() => handleClick(card.id)}
+            data-bs-toggle="modal"
+            data-bs-target="#deleteModal"
           >
             Delete
           </button>
+
+          <div className="modal fade" id="deleteModal" tabIndex="-1">
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-1 text-danger fw-bold">
+                    Deleting...
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body fs-5 fw-semibold">
+                  <span className="fw-bolder"> {card.title}</span>
+                  <p className="text-danger fw-bolder">
+                    This Post will be Erased Permantly!
+                  </p>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    data-bs-dismiss="modal"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    onClick={() => handleClick(card.id)}
+                    data-bs-dismiss="modal"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
